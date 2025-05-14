e96fc4e1b224b6c8f4e0eaccd4685d3f89a857be
Arnau <arnausatorra@gmail.com>
2025-05-14 21:12:43 +0200

    lab finished

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
 const changeAnimal = () => {
     const animalSelected = document.querySelector('#animalSelector');
-    const titleTxt = document.querySelector('#titleTxt')
-    const opened = document.querySelector('#openedDoor');
-    const closed = document.querySelector('#closedDoor');
+    const titleTxt = document.querySelector('#titleTxt');
+    const openedDoor = document.querySelector('#openedDoor');
+    const closedDoor = document.querySelector('#closedDoor');
     const dog = document.querySelector('#dog');
     const cat = document.querySelector('#cat');
-    const duck = document.querySelector('#duck');
-
-
+    const canary = document.querySelector('#canary');
+  
+    openedDoor.style.display = 'none';
+    closedDoor.style.display = 'none';
+    dog.style.display = 'none';
+    cat.style.display = 'none';
+    canary.style.display = 'none';
+  
     switch (animalSelected.value) {
-        case 'closedDoor':
-            titleTxt.innerText = 'Elige un animal';
-            openedDoor.style.display = 'none';
-            dog.style.display = 'none';
-            cat.style.display = 'none';
-            duck.style.display = 'none';
-            break;  
-        case 'dog':
-            titleTxt.innerText = 'Has elegido un perro';
-            dog.style.display = 'inline';
-            openedDoor.style.display = 'inline';
-            break;
-        case 'cat':
-            titleTxt.innerText = 'Has elegido un gato';
-            cat.style.display = 'inline';
-            openedDoor.style.display = 'inline';
-
-            break;
-        case 'duck':
-            titleTxt.innerText = 'Has elegido un pato'; 
-            duck.style.display = 'inline';
-            openedDoor.style.display = 'inline';
-
-            break;
-        default:
-            titleTxt.innerText = 'Elige un animal';
-            openedDoor.style.display = 'none';
-            dog.style.display = 'none';
-            cat.style.display = 'none';
-            duck.style.display = 'none';
-            break;
+      case 'dog':
+        titleTxt.innerText = 'Has elegido un perro';
+        openedDoor.style.display = 'inline';
+        dog.style.display = 'inline';
+        break;
+      case 'cat':
+        titleTxt.innerText = 'Has elegido un gato';
+        openedDoor.style.display = 'inline';
+        cat.style.display = 'inline';
+        break;
+      case 'canary':
+        titleTxt.innerText = 'Has elegido un canario';
+        openedDoor.style.display = 'inline';
+        canary.style.display = 'inline';
+        break;
+      default:
+        titleTxt.innerText = 'Elige un animal';
+        closedDoor.style.display = 'inline';
+        break;
     }
-}
+  };
+  
